5e4a1f57183272523d23bd98b546752688ad3f42
nils b <nils_christopher.boeschen@tu-darmstadt.de>
2026-04-07 16:43:40 +0200

    fixed logic to allow for appear/hide of groups of objects

--- slideio.py
+++ slideio.py
@@ -27,6 +27,19 @@ def save_diagram(tree, outfile):
         xmlstring = etree.tostring(tree, encoding="unicode", pretty_print=True)
         file.write(xmlstring)
 
+def safe_remove(page, element):
+    # first remove all elements that are children of the element in the current page
+    for w in page.xpath(f".//*[@parent='{element.attrib['id']}']"):
+        if w.getparent().tag == "object":
+            # special case: some images use an extra layer of tag "object" ...
+            tmp = w.getparent().getparent()
+            tmp.remove(w.getparent())
+        else:
+            w.getparent().remove(w)
+    # remove the element
+    element.getparent().remove(element)
+
+
 def expand(tree):
     """
     Handle "appear" and "hide" attributes
@@ -35,48 +48,47 @@ def expand(tree):
     skipped_pages = 0
 
     # Iterate all "diagram" types in xml tree. These are all pages in drawio, and will be the slides of the pdf.
-    for page_index, x in enumerate(tree.xpath(".//diagram")):
+    for page_index, orig_page in enumerate(tree.xpath(".//diagram")):
 
         # skip pages containing any object with the "skip_page" property
-        if len(x.xpath(".//*[@skip_page]")) != 0:
+        if len(orig_page.xpath(".//*[@skip_page]")) != 0:
             skipped_pages += 1
-            x.getparent().remove(x)
+            orig_page.getparent().remove(orig_page)
             continue
 
         # find maximum "appear"/"hide" value:
         max_expand = 0
-        for z in x.xpath(".//*[@appear|@hide]"):
+        for z in orig_page.xpath(".//*[@appear|@hide]"):
             max_expand = max(max_expand, int(z.get("appear",0)))
             max_expand = max(max_expand, int(z.get("hide",0)))
 
         added_pages += max_expand
 
         # add page number to "page_number" elements
-        for z in x.xpath(".//*[@slide_number]"):
+        for z in orig_page.xpath(".//*[@slide_number]"):
             z.attrib["label"] = f"{page_index-skipped_pages+1}"
 
-        # add a duplicate of the full diagram max_expand times
+        # add a duplicate of the full diagram max_expand+1 times
         # the first one added will be the last one in the final file
-        parent = x.getparent()
-        for i in range(max_expand,0,-1):
+        parent = orig_page.getparent()
+        for i in range(max_expand,-1,-1):
             # i is the current frame of the slide
-            y = deepcopy(x)
+            copied_page = deepcopy(orig_page)
             # change diagram id
-            y.attrib["id"] = y.attrib["id"] + "X"*i
+            copied_page.attrib["id"] = copied_page.attrib["id"] + "X"*i
             # remove "appear" elements
-            for z in y.xpath(".//*[@appear]"):
+            for z in copied_page.xpath(".//*[@appear]"):
                 if i < int(z.attrib["appear"]):
-                    z.getparent().remove(z)
+                    safe_remove(copied_page,z)
             # remove "hide" elements
-            for z in y.xpath(".//*[@hide]"):
+            for z in copied_page.xpath(".//*[@hide]"):
                 if i >= int(z.attrib["hide"]):
-                    z.getparent().remove(z)
-            parent.insert(parent.index(x)+1, y)
+                    safe_remove(copied_page,z)
+            parent.insert(parent.index(orig_page)+1, copied_page)
 
-        # remove "appear" elements from original page
-        for z in x.xpath(".//*[@appear]"):
-            if int(z.attrib["appear"]) > 0:
-                z.getparent().remove(z)
+        # remove original page
+        parent.remove(orig_page)
+
 
     # update number of pages
     tree.attrib["pages"] = str(int(tree.attrib["pages"]) + added_pages - skipped_pages)
